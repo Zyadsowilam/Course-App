50f0711fe59c95392055a032188de0432476ca5d
Ahmed-Amrr <ahmed2000amr@yahoo.com>
2022-03-20 18:05:12 +0200

    created a VS project
    created a new VS project  to start working with

---
+++ course_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//Add your routes and providers in MaterialApp
+//Build your pages in different files
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
