29b71fc425f0f2a51f1f5164f89e6339365cb2d4
Ahmed-Amrr <ahmed2000amr@yahoo.com>
2022-03-21 22:03:14 +0200

    Revert "test"
    This reverts commit bac040877c0606b63eb88ea8817f6600a55f0945.

--- course_app/lib/home.dart
+++ course_app/lib/home.dart
@@ -1,11 +1,7 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import './home.dart';
 import 'package:provider/provider.dart';
 import 'nav.dart';
-import 'card.dart';
-import 'testdata.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,11 +11,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  DummyData dummydata = DummyData();
   @override
   Widget build(BuildContext context) {
-    double screenheight = MediaQuery.of(context).size.height;
-    double screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 120.10,
@@ -45,29 +38,6 @@ class _HomeState extends State<Home> {
         )),
       ),
       drawer: SideNav(),
-      body: Container(
-        width: screenwidth,
-        height: screenheight,
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Container(
-              color: Colors.white,
-              padding: EdgeInsets.all(3),
-              height: 30,
-              child: Text(
-                "Home",
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-              )),
-          ListView.builder(
-              itemCount: dummydata.product.length,
-              itemBuilder: (BuildContext context, int index) {
-                return CustomCard(
-                    courseName: dummydata.product[index].productName,
-                    enroll: true,
-                    image: dummydata.product[index].image);
-              })
-        ]),
-      ),
     );
   }
 }
